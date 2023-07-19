6ac6edfb52c0181dcbf8530c9dc5995042052bea
André Théo LAURET <andrelauret@icloud.com>
2023-07-19 09:12:50 +0400

    0.0.8

--- xnbtd/plannings/templatetags/events.py
+++ xnbtd/plannings/templatetags/events.py
@@ -9,6 +9,23 @@ register = template.Library()
 
 
 @register.simple_tag
-def get_upcoming_events(count):
+def get_upcoming_events_grouped(count):
     yesterday = datetime.now() - timedelta(days=1)
-    return Event.objects.filter(date__gt=yesterday)[:count]
+    events = Event.objects.filter(date__gt=yesterday)[:count]
+
+    grouped_events = {}
+    for event in events:
+        event_date = event.date
+        if event_date == datetime.now().date():
+            date_key = "Aujourd'hui"
+        elif event_date == (datetime.now() + timedelta(days=1)).date():
+            date_key = "Demain"
+        else:
+            date_key = event_date
+
+        if date_key in grouped_events:
+            grouped_events[date_key].append(event)
+        else:
+            grouped_events[date_key] = [event]
+
+    return grouped_events
