0adc152df68ed4c759a7e68e333ec8740afd73fd
eldertek <andrelauret@eclipse-technology.eu>
2025-01-04 17:03:16 +0100

    update 2 files and create 2 files

--- xnbtd/tours/models.py
+++ xnbtd/tours/models.py
@@ -3,17 +3,16 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.utils import formats
-from django.utils.translation import gettext_lazy as _
 
 
 class BaseModel(models.Model):
-    linked_user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=_("Deliveryman"))
-    name = models.CharField(max_length=255, verbose_name=_("Tour Name"))
-    date = models.DateField(verbose_name=_("Tour Date"))
-    beginning_hour = models.TimeField(verbose_name=_("Beginning Hour"))
-    ending_hour = models.TimeField(verbose_name=_("Ending Hour"))
-    license_plate = models.CharField(max_length=7, verbose_name=_("License Plate"))
-    comments = models.TextField(verbose_name=_("Comments"), null=True, blank=True)
+    linked_user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="livreur")
+    name = models.CharField(max_length=255, verbose_name="numéro de tournée")
+    date = models.DateField(verbose_name="date")
+    beginning_hour = models.TimeField(verbose_name="début de la journée")
+    ending_hour = models.TimeField(verbose_name="fin de la journée")
+    license_plate = models.CharField(max_length=7, verbose_name="Plaque d'immatriculation")
+    comments = models.TextField(verbose_name="Commentaires", null=True, blank=True)
 
     def save(self, *args, **kwargs):
         self.license_plate = self.license_plate.upper()
@@ -28,76 +27,77 @@ class BaseModel(models.Model):
 
 
 class GLS(BaseModel):
-    points_charges = models.IntegerField(verbose_name=_("Points Charges"))
-    points_delivered = models.IntegerField(verbose_name=_("Points Delivered"))
-    packages_charges = models.IntegerField(verbose_name=_("Packages Charges"))
-    packages_delivered = models.IntegerField(verbose_name=_("Packages Delivered"))
-    avp_relay = models.IntegerField(verbose_name=_("AVP Relay"))
-    shd = models.IntegerField(verbose_name=_("SHD"))
-    eo = models.IntegerField(verbose_name=_("EO"))
-    pickup_point = models.IntegerField(verbose_name=_("Pickup Point"))
-    full_km = models.IntegerField(verbose_name=_("Full KM"))
+    points_charges = models.IntegerField(verbose_name="Points chargés")
+    points_delivered = models.IntegerField(verbose_name="Points livrés")
+    packages_charges = models.IntegerField(verbose_name="Colis chargés")
+    packages_delivered = models.IntegerField(verbose_name="Colis livrés")
+    avp_relay = models.IntegerField(verbose_name="AVP Relais")
+    shd = models.IntegerField(verbose_name="SHD")
+    eo = models.IntegerField(verbose_name="EO")
+    pickup_point = models.IntegerField(verbose_name="Colis ramassés")
+    full_km = models.IntegerField(verbose_name="Plein / KM")
 
     class Meta:
-        verbose_name = _("GLS")
-        verbose_name_plural = _("GLS")
+        verbose_name = "GLS"
+        verbose_name_plural = "GLS"
 
 
 class ChronopostDelivery(BaseModel):
-    charged_packages = models.IntegerField(verbose_name=_("Charged Packages"))
-    charged_points = models.IntegerField(verbose_name=_("Charged Points"))
-    including_ip = models.IntegerField(verbose_name=_("Including IP"))
-    relay = models.IntegerField(verbose_name=_("Relay"))
-    return_packages = models.IntegerField(verbose_name=_("Return Packages"))
-    return_points = models.IntegerField(verbose_name=_("Return Points"))
-    overdue = models.IntegerField(verbose_name=_("Overdue"))
-    anomalies = models.IntegerField(verbose_name=_("Anomalies"))
-    total_points = models.IntegerField(verbose_name=_("Total Points"))
-    full_km = models.IntegerField(verbose_name=_("Full KM"))
+    charged_packages = models.IntegerField(verbose_name="Packages chargés")
+    charged_points = models.IntegerField(verbose_name="Points chargés")
+    including_ip = models.IntegerField(verbose_name="Inclus IP")
+    relay = models.IntegerField(verbose_name="Relais")
+    return_packages = models.IntegerField(verbose_name="Packages retournés")
+    return_points = models.IntegerField(verbose_name="Points retournés")
+    overdue = models.IntegerField(verbose_name="En retard")
+    anomalies = models.IntegerField(verbose_name="Anomalies")
+    total_points = models.IntegerField(verbose_name="Total de points")
+    full_km = models.IntegerField(verbose_name="Plein / KM")
 
     class Meta:
-        verbose_name = _("Chronopost Delivery")
-        verbose_name_plural = _("Chronopost Delivery")
+        verbose_name = "Chronopost - Livraison"
+        verbose_name_plural = "Chronopost - Livraison"
 
 
 class ChronopostPickup(BaseModel):
-    esd = models.IntegerField(verbose_name=_("ESD"))
-    picked_points = models.IntegerField(verbose_name=_("Picked Points"))
-    poste = models.IntegerField(verbose_name=_("Poste"))
+    esd = models.IntegerField(verbose_name="ESD")
+    picked_points = models.IntegerField(verbose_name="Points collectés")
+    poste = models.IntegerField(verbose_name="Poste")
 
     class Meta:
-        verbose_name = _("Chronopost Pickup")
-        verbose_name_plural = _("Chronopost Pickup")
+        verbose_name = "Chronopost - Ramasse"
+        verbose_name_plural = "Chronopost - Ramasse"
 
 
 class TNT(BaseModel):
-    client_numbers = models.IntegerField(verbose_name=_("Client numbers"))
-    refused = models.IntegerField(verbose_name=_("Refused"))
-    avp = models.IntegerField(verbose_name=_("avp"))
-    cad = models.IntegerField(verbose_name=_("cad/return"))
-    totals_clients = models.IntegerField(verbose_name=_("totals clients"))
-    occasional_abductions = models.IntegerField(verbose_name=_("occasional abductions"))
-    regular_abductions = models.IntegerField(verbose_name=_("regular abductions"))
-    totals_clients_abductions = models.IntegerField(verbose_name=_("total clients abductions"))
-    kilometers = models.IntegerField(verbose_name=_("KM/Full"))
+    client_numbers = models.IntegerField(verbose_name="Nombre de clients")
+    refused = models.IntegerField(verbose_name="Refusés")
+    avp = models.IntegerField(verbose_name="avp")
+    cad = models.IntegerField(verbose_name="cad/retour")
+    totals_clients = models.IntegerField(verbose_name="Total de clients")
+    occasional_abductions = models.IntegerField(verbose_name="abductions occasionnelles")
+    regular_abductions = models.IntegerField(verbose_name="abductions régulières")
+    totals_clients_abductions = models.IntegerField(verbose_name="Total d'abductions de clients")
+    kilometers = models.IntegerField(verbose_name="KM/Plein")
 
     class Meta:
-        verbose_name = _("TNT")
-        verbose_name_plural = _("TNT")
+        verbose_name = "TNT"
+        verbose_name_plural = "TNT"
 
 
 class Ciblex(BaseModel):
     type = models.CharField(max_length=255, default="LIVRAISONS")
-    nights = models.IntegerField(verbose_name=_("nights"))
-    days = models.IntegerField(verbose_name=_("days"))
-    avp = models.IntegerField(verbose_name=_("avp"))
-    spare_part = models.IntegerField(verbose_name=_("spare part"))
-    synchro = models.IntegerField(verbose_name=_("synchro"))
-    morning_pickup = models.IntegerField(verbose_name=_("morning pickups"))
+    nights = models.IntegerField(verbose_name="nuit")
+    days = models.IntegerField(verbose_name="jour")
+    avp = models.IntegerField(verbose_name="avp")
+    spare_part = models.IntegerField(verbose_name="pièce de rechange")
+    synchro = models.IntegerField(verbose_name="synchronisation")
+    morning_pickup = models.IntegerField(verbose_name="ramassages matinaux")
+    relais = models.IntegerField(verbose_name="relais")
 
     class Meta:
-        verbose_name = _("Ciblex")
-        verbose_name_plural = _("Ciblex")
+        verbose_name = "Ciblex"
+        verbose_name_plural = "Ciblex"
 
 
 class BreakTime(models.Model):
@@ -105,12 +105,12 @@ class BreakTime(models.Model):
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey("content_type", "object_id")
 
-    start_time = models.TimeField(verbose_name=_("Start of Break"))
-    end_time = models.TimeField(verbose_name=_("End of Break"))
+    start_time = models.TimeField(verbose_name="Début de la pause")
+    end_time = models.TimeField(verbose_name="Fin de la pause")
 
     def __str__(self):
-        return str(_("Break from {} to {}")).format(self.start_time, self.end_time)
+        return "Pause de {} à {}".format(self.start_time, self.end_time)
 
     class Meta:
-        verbose_name = _("Break Time")
-        verbose_name_plural = _("Break Times")
+        verbose_name = "Pause"
+        verbose_name_plural = "Pauses"
